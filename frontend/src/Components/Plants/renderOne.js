a476105c77087410b2969e9bb23a567bcd0f5b7f
Silyu Li <silyu.li@rwth-aachen.de>
2023-03-30 09:35:31 +0200

    Used Bootstrap Tooltip

--- frontend/src/Components/Plants/renderOne.js
+++ frontend/src/Components/Plants/renderOne.js
@@ -3,13 +3,16 @@ import Bushy from './bushy'
 import Dragon from './dragonTree'
 import Pilea from './pilea'
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
-const Render = ({plant, seed, status, title, progress}) => {
+const Render = ({plant, seed, status, title, progress, start, due}) => {
     let p = plant
     let s = seed
     let t = status
     let i = title
     let r = progress
+    let st = start
+    let d = due
     const renderRandomPlant = () => {
         switch(true) {
             case (p === "Zamia" && t === "To Do"):
@@ -17,14 +20,14 @@ const Render = ({plant, seed, status, title, progress}) => {
             case (p === "Zamia" && t === "Doing"):
                 return <Zamia seedd={s} status={r} />
             case (p === "Zamia" && t === "Finished"):
-                return <Zamia seedd={s} status={progress} />
+                return <Zamia seedd={s} status={r} />
 
             case (p === "Bushy" && t === "To Do"):
-                    return <Bushy seedd={s} status={progress} />
+                    return <Bushy seedd={s} status={r} />
             case (p === "Bushy" && t === "Doing"):
-                    return <Bushy seedd={s} status={progress} />
+                    return <Bushy seedd={s} status={r} />
             case (p === "Bushy" && t === "Finished"):
-                    return <Bushy seedd={s} status={progress} />
+                    return <Bushy seedd={s} status={r} />
 
             case (p === "Dragon" && t === "To Do"):
                     return <Dragon seedd={s} status={r} />
@@ -34,17 +37,18 @@ const Render = ({plant, seed, status, title, progress}) => {
                     return <Dragon seedd={s} status={r} />
 
             case (p === "Pilea" && t === "To Do"):
-                    return <Pilea seedd={s} status={progress} />
+                    return <Pilea seedd={s} status={r} />
             case (p === "Pilea" && t === "Doing"):
-                    return <Pilea seedd={s} status={progress} />
+                    return <Pilea seedd={s} status={r} />
             case (p === "Pilea" && t === "Finished"):
-                    return <Pilea seedd={s} status={progress} />
+                    return <Pilea seedd={s} status={r} />
             default: 
                 return null   
         }
     }
 
     return(
+        <OverlayTrigger placement="top" overlay={<Tooltip><div>Start: {(new Date(st)).toLocaleDateString("en-GB")}</div> <div>Due: {(new Date(d)).toLocaleDateString("en-GB")}</div></Tooltip>}>
         <div className='card border-success' style={{width: '250px', margin: '10px 10px 10px 10px'}}>
             <div className='card-img-top text-center' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>{renderRandomPlant()}</div>
             <hr />    
@@ -57,6 +61,7 @@ const Render = ({plant, seed, status, title, progress}) => {
                 </div>*/}
             </div>
         </div>
+        </OverlayTrigger>
     )
 }
 export default Render
--- frontend/src/Components/plantContainer.js
+++ frontend/src/Components/plantContainer.js
@@ -12,6 +12,8 @@ const Pcontainer = () => {
     const [title, setTitle] = useState(null)
     const [count, setCount] = useState(0)
     const [progress, setProgress] = useState([])
+    const [due, setDue] = useState([])
+    const [start, setStart] = useState([])
     //const memo = useMemo(() => fetchData(),seeds)
 
     useEffect(() => {
@@ -39,18 +41,24 @@ const Pcontainer = () => {
             let P = []
             let T = []
             let E = []
+            let Due = []
+            let Sta = []
             let i = 0
             while(i < json.length) {
                 S.push(json[i].seed)
                 P.push(json[i].plant)
                 T.push("Finished")
                 E.push(json[i].title)
+                Due.push(json[i].endDate)
+                Sta.push(json[i].startDate)
                 i++
             }
             setPlant(P)
             setSeed(S)
             setStatus(T)
             setTitle(E)
+            setDue(Due)
+            setStart(Sta)
             setCount(S.length)
             }
             return() => {
@@ -63,7 +71,9 @@ const Pcontainer = () => {
     const renderPlant = ()=> {
         let PT = []
         for(let i = 0; i < count; i++) {
-            PT.push(<Render className='col' plant={plants[i]} seed={seeds[i]} status = {status[i]} title = {title[i]} progress = {progress[i]*100} key={i} />) //, overflowY: 'scroll' 
+            PT.push(
+              <Render className='col' plant={plants[i]} seed={seeds[i]} status = {status[i]} title = {title[i]} progress = {progress[i]*100} key={i} start = {start[i]} due={due[i]}/>
+              ) //, overflowY: 'scroll' 
         }
         return PT
     }
