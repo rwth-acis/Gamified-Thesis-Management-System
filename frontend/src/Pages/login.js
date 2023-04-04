737b451a1aabc4d01a889c49c2cf3d0ff7bd8a94
Silyu Li <silyu.li@rwth-aachen.de>
2023-04-04 18:18:03 +0200

    Switched to localtestclient for now

--- frontend/src/Pages/login.js
+++ frontend/src/Pages/login.js
@@ -3,7 +3,7 @@ import "openidconnect-signin/openidconnect-signin-react.js"
 const Login = () => {
 
     return(
-        <openidconnect-signin-react oidcClientId="BA_Silyu_Li" popupRedirectUri="http://localhost:3000/"></openidconnect-signin-react>
+        <openidconnect-signin-react oidcClientId="localtestclient" popupRedirectUri="http://localhost:3000/"></openidconnect-signin-react>
     )
 }
 export default Login
