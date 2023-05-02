36a242608d2a9dcbc0929fe3c0b304331f9e6aa3
Silyu Li <silyu.li@rwth-aachen.de>
2023-05-03 00:07:05 +0200

    Refresh after sign in

--- frontend/src/Pages/loginPage.js
+++ frontend/src/Pages/loginPage.js
@@ -1,21 +1,29 @@
 import Login from "./login"
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 //import Button from "react-bootstrap/esm/Button";
 
 const LoginPage = () => {
 
+    const handleClick = () => {
+        window.location.reload()
+    }
+
     return (
         <Container fluid style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh"}}>
             <Col>
-            <Row>
+              <Row>
                 <h3 className="text-center" >Welcome to the thesis management system of the RWTH Informatik 5, please sign in to continue</h3>
-            </Row>
-            <br />
-            <Row>
-                <Login />
-            </Row> </Col>   
+              </Row>
+              <br />
+              <Row>
+                  <Login />
+                  <hr />
+                  <a href="" className="text-center" onClick={handleClick}>Already Signed In? Click Here To Refresh!</a>
+              </Row> 
+            </Col>   
         </Container>
     )
 }
