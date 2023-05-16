f71b7a9d59361bbcbae301c330f02105aea1cd8e
Silyu Li <silyu.li@rwth-aachen.de>
2023-05-16 17:16:36 +0200

    small change

--- frontend/src/Pages/allStudents.js
+++ frontend/src/Pages/allStudents.js
@@ -75,7 +75,7 @@ const All = () => {
             <Row>
             { users ?
             users.map(user => (
-                <Col key={user._id} sm={2} style={{ marginBottom: '10px' }}>
+                <Col key={user._id} sm={2} md={3} style={{ marginBottom: '10px' }}>
                 <Card style={{margin: '10px 10px 10px 10px',boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.5)', cursor: 'pointer', width: '200px', height: '100%'}}
                        onClick={()=>[handleClick(user._id, user.firstName, user.lastName, user.workName, user.workType),openModal()]}>
                     {
